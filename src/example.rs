938cfe9c2ce8925b653069dfe265547db35dcc9d
zhanyan <1075465357lin@gmail.com>
2024-08-15 23:22:42 +0800

    mod: hello

--- src/example.rs
+++ src/example.rs
@@ -1,2 +1,3 @@
 pub mod guess_number;
 pub mod shadowing;
+pub mod say;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use rust_tech::example::{
+    say::hello,
     guess_number::guess_number,
     shadowing::shadowing
 };
 
 fn main() {
 
+    hello();
     guess_number();
     shadowing();
 
