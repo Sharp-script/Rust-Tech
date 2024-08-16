aa9e4605d8886cc86deb0d560228506a67a30b51
zhanyan <1075465357lin@gmail.com>
2024-08-17 00:33:50 +0800

    simple rust axum web server

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,62 @@
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::{TcpListener}
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
 };
-use rust_tech::example::{
-    say::hello
-};
+use serde::{Deserialize, Serialize};
+use tokio::net::TcpListener;
+use tracing::info;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    hello();
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+async fn main() {
+    tracing_subscriber::fmt::init();
 
-    loop {
-        let (mut socket, _) = listener.accept().await?;
+    axum::serve(listener_bind().await, routers_build()).await.unwrap();
+    info!("Server finish init !");
+}
 
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
+fn routers_build() -> Router {
+    let routers = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
 
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    // socket closed
-                    Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
-                        return;
-                    }
-                };
+    info!("Server app router has init");
 
-                // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
-                }
-            }
-        });
-    }
+    routers
+}
+
+async fn listener_bind() -> TcpListener {
+    let mut ip = String::new();
+    let server_port = "6001";
+    ip.push_str("127.0.0.1:");
+    ip.push_str(server_port);
+    let listener = TcpListener::bind(ip.clone()).await.unwrap();
+
+    info!("Server listener has init: {}", ip);
+
+    listener
+}
+
+async fn root() -> &'static str {
+    "Hello, Rust Axum 'web application framework' !"
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
+    let user = User {
+        id: rand::random(),
+        username: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
 }
